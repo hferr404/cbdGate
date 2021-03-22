da58cc91a2c45711380ce3361f890fee7532ab8b
AlexBesle <alex_besle@hotmail.com>
2021-03-22 17:59:04 +0100

    besle220321

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Membre;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/inscription", name="security_inscription")
      */
-    public function index(): Response
+
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+     {
+    $membre = new Membre(); 
+  
+    $form = $this->createForm(InscriptionType::class, $membre); 
+
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid())
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+    
+    $hash = $encoder->encodePassword($membre, $membre->getPassword());
+
+    $membre->setPassword($hash); 
+
+    $membre->setRoles(["ROLE_USER"]);
+
+    $manager->persist($membre);
+   
+    $manager->flush(); 
+    }
+   
+
+    return $this->render('security/inscription.html.twig' ,[
+        'form' => $form->createView()
+    ]);
+  
     }
 }
