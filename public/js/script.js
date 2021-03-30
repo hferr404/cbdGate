f3c813d6b2095eeb39d29d75f36a66b046c730e1
Jordan <jordan_mendes@hotmail.fr>
2021-03-30 12:51:21 +0200

    'petitemodifvue'

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
       $("#men_ex").animate({width:"toggle"});
     });
 
+    $('#jojo').on({
+      'click': function() {
+           var src = ($(this).attr('src') === 'image/arrowleft.png')
+              ? 'image/arrow.png'
+              : 'image/arrowleft.png';
+           $(this).attr('src', src);
+      }
+    });
 
  
 });
